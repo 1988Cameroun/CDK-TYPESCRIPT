b5edaef15c0982ff9bb51e953fa7d44caed8133f
Idriss Yann Tchokogue <105473976+1988Cameroun@users.noreply.github.com>
2023-05-09 21:59:19 -0500

    Update cdk-demo-stack.ts

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,35 +1,31 @@
-import { Vpc } from '@aws-cdk/aws-ec2/lib/vpc';
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { SubnetType } from 'aws-cdk-lib/aws-ec2';
+import { Vpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import { Vpc, SubnetType } from '@aws-cdk/aws-ec2';
+import { Stack, StackProps } from 'aws-cdk-lib';
 
 export class CdkDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-     // CHANGE: We have created the vpc object from the Vpc class.
-     const vpc = new Vpc(this, 'MainVpc',{
-    
-      // CHANGE: this is where we define how many AZs to use
+    // CHANGE: We have created the vpc object from the Vpc class.
+    const vpc = new Vpc(this, 'MainVpc', {
+      cidr: '10.0.0.0/16',
       maxAzs: 2,
-        
-     // CHANGE: We define a single subnet configuration per AZ.
-        subnetConfiguration:  [
-          {
-            // CHANGE: this is it's CIDR mask so 255.255.255.0
-            cidrMask: 24,
-  
-            // CHANGE: a name for each of these subnets
-            name: 'public-subnet',
-  
-            // CHANGE: and the subnet type to be used - here we will have
-            // a public subnet. There are other options available here.
-            subnetType: SubnetType.PUBLIC
-          },
-        ]
-      });
-    }
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: SubnetType.PRIVATE,
+        },
+      ],
+    });
+
+    // More stack definition code goes here
   }
-              
+}
+   
